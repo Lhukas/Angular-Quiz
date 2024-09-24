1da7116ba6d0c38f904e1467167d068616e35c19
Lhukas <58892260+Lhukas@users.noreply.github.com>
2024-09-24 12:28:20 +0200

    maj

--- src/app/quiz/categorie/categorie.component.ts
+++ src/app/quiz/categorie/categorie.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {QuizService} from "../../shared/services/quiz.service";
 import {CategorieService} from "../../shared/services/categorie.service";
 import {compareSegments} from "@angular/compiler-cli/src/ngtsc/sourcemaps/src/segment_marker";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-categorie',
@@ -13,11 +13,17 @@ export class CategorieComponent implements OnInit{
 
   constructor(private categorieService : CategorieService,
               private router: Router,
-              private quizService : QuizService) { }
+              private quizService : QuizService,
+              private route: ActivatedRoute) { }
 
+  playerName : string =""
   quizCategorie: any[] = this.categorieService.quizCategorie;
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.playerName = params['playerName'];
+    });
+
     this.categorieService.resetQuiz()
     this.categorieService.getCategorie();
     this.quizService.resetQuiz()
